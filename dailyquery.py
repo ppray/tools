940e68ed2c1a8f616ea347ba5faa44dd9a92a074
EC2 Default User <ec2-user@ip-172-31-16-6.ap-southeast-1.compute.internal>
2018-04-02 19:14:12 +0800

    daily query use Class miner; add roi in database.

--- dailyquery.py
+++ dailyquery.py
@@ -3,8 +3,71 @@
 
 import requests
 import json
+import os
 import sqlite3
 
+class miner:
+    #定义基本属性
+    hs=0
+    power=0
+    price=0
+    btcprice=0
+    nhr = 0
+    ele = 0 #Electricity
+
+    def __init__(self,name,hs,pw,pr,bpr,nhr):
+        self.hs = hs
+        self.name = name
+        self.power = pw
+        self.price = pr # miner price
+        self.btcprice = bpr # btc price
+        self.nhr = nhr # unit G
+
+    def roi(self):
+        i=1
+        roi = 720
+        total = 0
+        cny = self.btcprice *6.4 #btc price CNY
+        bestprice = self.price
+        
+        print "\n",self.name,"当前售价",self.price
+        while (i <= 102):
+            btc_2weeks_income_per_100T = 1800.0 * 14 * 100000 / self.nhr
+            btc_daily_income_per_100T = 1800.0 * 100000 / self.nhr
+            total = total + btc_2weeks_income_per_100T
+            if (total/100*self.hs*cny >= self.price+self.power/1000*24*0.5*14*i):
+                roi = i*14 - ((total/100*13*cny) - self.price)/(btc_daily_income_per_100T*cny)
+                break
+            #print i, "th 2weeks total is", total/100*self.hs, total/100*self.hs*cny , "network_hashrate_G is", network_hashrate_G,"cost is", self.price+self.power/1000*24*0.5*14*i
+            if i == 13:
+                total_btc_half_year = total/100*self.hs
+                total_cny_half_year = total_btc_half_year*cny
+                total_ele_half_year = self.power/1000*24*0.5*14*i
+                total_cost_cny_half_year = self.power/1000*24*0.5*14*i+self.price
+                total_cost_bit_half_year = total_cost_cny_half_year/cny
+                bestprice = int(total_cny_half_year - self.power/1000*24*0.5*14*i)
+                print self.name,"建议购入价格",bestprice,"RMB"
+                #print self.name,"挖矿半年收益：",total_cny_half_year,"RMB (",total_btc_half_year,"btc), 成本",total_cost_cny_half_year,"RMB(", total_cost_bit_half_year,"btc)"
+            if i == 26:
+                total_btc_one_year = total/100*self.hs
+                total_cny_one_year = total/100*self.hs*cny
+                total_ele_one_year = self.power/1000*24*0.5*14*i
+                total_cost_cny_one_year = self.power/1000*24*0.5*14*i+self.price
+                total_cost_bit_one_year = total_cost_cny_one_year/cny
+                print self.name,"挖矿一年收益：",total_cny_one_year,"RMB (",total_btc_one_year,"btc), 成本",total_cost_cny_one_year,"RMB(", total_cost_bit_one_year,"btc)"
+            self.nhr = self.nhr*1.07
+            i = i + 1 
+        if roi==720:
+            print "回本周期: 无法回本"
+        else:
+            print "回本周期: ", roi,"天"
+        return bestprice,roi
+
+    def speak(self):  
+        print("%s is speaking: I am %d years old" %(self.hs,self.price))
+
+    def bestprice(self):  
+        print ""        
 
 r = requests.get("https://api.huobipro.com/market/detail?symbol=btcusdt")
 #print r.content
@@ -15,31 +78,22 @@ cny = price *6.4
 difficulty =  requests.get("https://blockchain.info/q/getdifficulty").content
 network_hashrate_G =  int(requests.get("https://blockchain.info/q/hashrate").content)
 nhg=network_hashrate_G
-btc_2weeks_income_per_100T = 1800.0 * 14 * 100000 / network_hashrate_G
+cost_ele_half_year = 13.44*7*26 #1400w
 
-i=1
-sum = 0
+##     hashrate power minerprice btcprice, nh, 
+t9 = miner("T9 10.5T",10.5,1432,5600,price,network_hashrate_G)
 
-while (i <=13 ):
-    btc_2weeks_income_per_100T = 1800.0 * 14 * 100000 / network_hashrate_G
-    sum = sum + btc_2weeks_income_per_100T
-    #print i, "th 2weeks sum is", sum, "network_hashrate_G is", network_hashrate_G
-    network_hashrate_G = network_hashrate_G*1.07
-    i = i + 1 
+s9_13 = miner("S9 13T",13,1280,9000,price,network_hashrate_G)
 
-income_cny_half_year_per_T = sum *cny /100
-income_btc_half_year_per_T = sum /100
-consume_ele_half_year = 13.44*7*26 #1400w
+s9_14 = miner("S9 14T",14,1372,10500,price,network_hashrate_G)
 
-print "比特币矿机购买价格指南：\n"
+t1 = miner("T1 16T",16,1480,17500,price,network_hashrate_G)
 
-hs = 4
-while (hs <=20):
-    #print income_cny_half_year_per_T, hs, consume_ele_half_year
-    print hs,"T 矿机价格低于", int(income_cny_half_year_per_T*hs - consume_ele_half_year), "元"
-    hs = hs +1
+v9 = miner("V9 4T",4,1027,1710,price,network_hashrate_G)
 
-print "\n量化分析参数，当前币价：",cny,"，当前难度", difficulty, " 全网算力：",nhg,"G，ROI：182，每次难度增长估算：7%，产量：",income_btc_half_year_per_T, "btc/T，电费：",consume_ele_half_year,"(1400w, 0.4元计)\n"
+
+print "\n量化分析参数，当前币价：",cny,"，当前难度", difficulty, " 全网算力：",nhg,"G，每次难度增长估算：7%，电费：0.5\n"
+
 
 
 btc_balance_of_bigboss =  int(requests.get("https://blockchain.info/q/addressbalance/3Cbq7aT1tY8kMxWLbitaG7yT6bPbKChq64").content)
@@ -54,8 +108,6 @@ c = conn.cursor()
 cursor =c.execute("select * from minerprice order by ID desc limit 1")
 for row in cursor:
     ID=row[0]
-    print "date time is:", row[5]
-    print "date time is:", row[4]
     print "date time is:", row[6]
 
 #print "current ID is", ID
@@ -65,39 +117,36 @@ now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 ##### Record s9 14t #####
 ID = ID +1
 #print "s9 ID is", ID
-price = int(income_cny_half_year_per_T*14 - consume_ele_half_year)
-if price<0:
-    price = 0
-qstr="INSERT INTO minerprice (ID,NAME,bestprice,date) VALUES (%s,'s9_14',%s,'%s')" % (ID,price,now)
+price,roi = s9_14.roi()
+qstr="INSERT INTO minerprice (ID,NAME,bestprice,date,roi) VALUES (%s,'s9_14',%s,'%s',%s)" % (ID,price,now,roi)
 print qstr
 c.execute(qstr);
 
 ##### Record s9 13t #####
 ID = ID +1
-price = int(income_cny_half_year_per_T*13 - consume_ele_half_year)
-qstr="INSERT INTO minerprice (ID,NAME,bestprice,date) VALUES (%s,'s9_13',%s,'%s')" % (ID,price,now)
+price,roi = s9_13.roi()
+qstr="INSERT INTO minerprice (ID,NAME,bestprice,date,roi) VALUES (%s,'s9_13',%s,'%s',%s)" % (ID,price,now,roi)
 c.execute(qstr);
 
 ##### Record t9 10.5t #####
 ID = ID +1
-price = int(income_cny_half_year_per_T*10.5 - consume_ele_half_year)
-qstr="INSERT INTO minerprice (ID,NAME,bestprice,date) VALUES (%s,'t9',%s,'%s')" % (ID,price,now)
+price,roi = t9.roi()
+qstr="INSERT INTO minerprice (ID,NAME,bestprice,date,roi) VALUES (%s,'t9',%s,'%s',%s)" % (ID,price,now,roi)
 c.execute(qstr);
 
 ##### Record v9 4t #####
 ID = ID +1
-price = int(income_cny_half_year_per_T*4 - consume_ele_half_year)
-qstr="INSERT INTO minerprice (ID,NAME,bestprice,date) VALUES (%s,'v9',%s,'%s')" % (ID,price,now)
+price,roi = v9.roi()
+qstr="INSERT INTO minerprice (ID,NAME,bestprice,date,roi) VALUES (%s,'v9',%s,'%s',%s)" % (ID,price,now,roi)
 c.execute(qstr);
 
 ##### Record Halong 16t #####
 ID = ID +1
-price = int(income_cny_half_year_per_T*16 - consume_ele_half_year)
-qstr="INSERT INTO minerprice (ID,NAME,bestprice,date) VALUES (%s,'halong',%s,'%s')" % (ID,price,now)
+price,roi = t1.roi()
+qstr="INSERT INTO minerprice (ID,NAME,bestprice,date,roi) VALUES (%s,'halong',%s,'%s',%s)" % (ID,price,now,roi)
 c.execute(qstr);
 
 
 conn.commit()
 print "Records created successfully";
 conn.close()
-
